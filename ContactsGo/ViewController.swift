258a54d8d6dae718c6865943d18f49772f9a3d92
Zipei Jerry Shi <jerryjerryszp@gmail.com>
2020-08-28 23:48:32 -0400

    Initial Commit

---
+++ ContactsGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsGo
+//
+//  Created by Jerry Shi on 2020-08-28.
+//  Copyright © 2020 jerryszp6116. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
